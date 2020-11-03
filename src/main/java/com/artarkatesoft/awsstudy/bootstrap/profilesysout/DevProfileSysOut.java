fe2dadbdf8d0ba51e6d245110d292bcd7ee2fbe2
Art <d.art.shishkin@gmail.com>
2020-11-03 12:01:40 +0200

    22. Spring Boot Profiles with YAML

--- src/main/java/com/artarkatesoft/awsstudy/bootstrap/profilesysout/DevProfileSysOut.java
+++ src/main/java/com/artarkatesoft/awsstudy/bootstrap/profilesysout/DevProfileSysOut.java
@@ -3,17 +3,20 @@ package com.artarkatesoft.awsstudy.bootstrap.profilesysout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("dev")
 public class DevProfileSysOut {
+
     @Autowired
-    public DevProfileSysOut(@Value("${com.artarkatesoft.profile.message}") String msg) {
+    public DevProfileSysOut(@Value("${com.artarkatesoft.profile.message}") String msg, Environment env) {
         System.out.println("############################");
         System.out.println("############################");
         System.out.println("##           DEV          ##");
         System.out.println(msg);
+        System.out.println(env.getProperty("com.artarkatesoft.profile.message2"));
         System.out.println("############################");
         System.out.println("############################");
     }
